9ccefcd217fafdf56bead5b1ed74cfcee6bc4e7f
3 <118930716+72-S@users.noreply.github.com>
2023-05-18 23:33:33 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,9 +4,13 @@ $extractPath = "$HOME\Documents\WinTroy"
 $subfolderName = "WinTroy-main"
 
 Invoke-WebRequest -Uri $repositoryUrl -OutFile $downloadPath
+
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory($downloadPath, $extractPath)
+
 $fullPath = Join-Path -Path $extractPath -ChildPath $subfolderName
 Set-Location -Path $fullPath
+
 Start-Process -FilePath "inst.bat" -NoNewWindow -Wait
 exit
+
